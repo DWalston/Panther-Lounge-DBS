a661252084fc9f981ddf469a1162f1776a2145c3
M Dattis <38086897+MssTake@users.noreply.github.com>
2024-02-19 12:08:13 -0500

    visual changes frontend

--- public/member/script.js
+++ public/member/script.js
@@ -1,8 +1,19 @@
-function checkIn() {}
-
-function name() {
-  document.getElementById("name").textContent = name;
+function checkIn() {
+  // warn the user that the item will not be officially "checked in"
+  alert(
+    "Thank you for checking in this item. This item will not be officially marked as 'checked in' until an officer confirms the item is returned"
+  );
 }
+
+function checkedOut() {
+  // if the user has checked out an item, the item's information will be filled out automatically
+  // otherwise, a message stating that the member has not checked out anything will be displayed
+}
+
+function item() {
+  // retrives and fills in item information such as name, ID, & condition
+}
+
 
 // copy id number
 function copyThis(number) {
@@ -10,14 +21,48 @@ function copyThis(number) {
 }
 
 // date autofill
-function theDate() {
-  var now = new Date();
-  var day = ("0" + now.getDate()).slice(-2);
-  var today =
-    now.getFullYear() + "-" + (now.getMonth() + 1).slice(-2) + "-" + day;
-  document.getElementById("coDate").textContent = today;
-  day = ("14" + now.getDate()).slice(-2);
-  document.getElementById("ciDate").textContent = today;
+function dates() {
+  var full = new Date();
+  var day = full.getDate();
+  var month = full.getMonth() + 1;
+  document.getElementById("coDate").value =
+    full.getFullYear() + "-" + month + "-" + day;
+  // Jan, Mar, May, Jul, Aug, Oct, & Dec
+  if (
+    (day + 14 > 31 && month === 1) ||
+    month === 3 ||
+    month === 5 ||
+    month === 8 ||
+    month === 10
+  ) {
+    day = day - 17;
+    if (month === 12) {
+      month = month - 11;
+    } else {
+      month = month + 1;
+    }
+  }
+  // Apr, Jun, Sept, & Nov
+  else if (
+    (day + 14 > 30 && month === 2) ||
+    month === 4 ||
+    month === 6 ||
+    month === 9 ||
+    month === 11
+  ) {
+    day = day - 16;
+    month = month + 1;
+  }
+  // Feb, leap
+  else if (full.getFullYear() % 2 && day + 14 > 29 && month === 2) {
+    day = day - 15;
+    month = month + 1;
+  }
+  // Feb
+  else {
+    day = day - 14;
+    month = month++;
+  }
+  document.getElementById("ciDate").value =
+    full.getFullYear() + "-" + month + "-" + day;
 }
-
-function seeItem() {}
--- public/script.js
+++ public/script.js
@@ -1,67 +1,7 @@
-// side nav
-function nav() {
-  if (document.getElementById("nav").style.display === "block") {
-    document.getElementById("nav").style.display = "none";
-    document.getElementsByClass("body").style.marginLeft = "0";
-  } else {
-    document.getElementById("nav").style.display = "block";
-    document.getElementByClass("body").style.marginLeft = "250px";
-  }
-}
-
-// login
-function logIn(username, password) {
-  // check if username or password matches anything in the database
-  // username and password should not equal true in final
-  if (username === true) {
-    if (password === true) {
-      return true;
-    } else {
-      alert("invalid password");
-    }
-  } else {
-    alert("invalid username");
-  }
-}
-
-// sending query from homepage to catalog searchbar
-function catalog() {
-  document.getElementById("searchQ").textContent;
-  location.href = "catalog.html";
-}
-
-// live search in searchbar
-function liveSearch(search, table) {
-  var input = document.getElementById(search);
-  var searchUpCase = input.ariaValueMax.toUpperCase();
-  var table = document.getElementById(table);
-  var tr = table.getElementsByTagName("tr");
-  for (var row = 0; row < tr.length; row++) {
-    var td = tr[row].getElementsByTagName("td")[0];
-    if (td) {
-      var txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(searchUpCase) > -1) {
-        tr[row].style.display = "";
-      } else {
-        tr[row].style.display = "none";
-      }
-    }
-  }
-}
-
-// show/hide checkboxes
-function boxDispl() {
-  if (document.getElementById("boxes").style.display === "none") {
-    document.getElementById("boxes").style.display = "block";
-  } else {
-    document.getElementById("boxes").style.display = "none";
-  }
-}
-
 // the itemName & ItemDescription boxes are checked by default
 function check() {
-  document.getElementById("itemName").checked = true;
-  document.getElementById("itemDescription").checked = true;
+  document.getElementById("itemNameBox").checked = true;
+  document.getElementById("itemDescriptionBox").checked = true;
 }
 
 // show/hide column
@@ -70,5 +10,93 @@ function columnVis(element) {
     document.getElementById(element).style.visibility = "collapse";
   } else {
     document.getElementById(element).style.visibility = "visible";
+  }
+}
+
+function columnVis2(element) {
+  if (document.getElementById(element).style.visibility === "collapse") {
+    document.getElementById(element).style.visibility = "visible";
+  } else {
+    document.getElementById(element).style.visibility = "collapse";
+  }
+}
+
+// reveal extra filters based on item type
+function itemType() {
+  if (document.getElementById("type").value == "game") {
+    document.getElementById("game").style.height = "auto";
+    document.getElementById("game").style.visibility = "visible";
+    document.getElementById("hardware").style.height = "0";
+    document.getElementById("hardware").style.visibility = "hidden";
+    document.getElementById("media").style.height = "0";
+    document.getElementById("media").style.visibility = "hidden";
+  } else if (document.getElementById("type").value == "hardware") {
+    document.getElementById("game").style.height = "0";
+    document.getElementById("game").style.visibility = "hidden";
+    document.getElementById("media").style.height = "0";
+    document.getElementById("media").style.visibility = "hidden";
+    document.getElementById("hardware").style.height = "auto";
+    document.getElementById("hardware").style.visibility = "visible";
+  } else if (document.getElementById("type").value == "media") {
+    document.getElementById("game").style.height = "0";
+    document.getElementById("game").style.visibility = "hidden";
+    document.getElementById("hardware").style.height = "0";
+    document.getElementById("hardware").style.visibility = "hidden";
+    document.getElementById("media").style.height = "auto";
+    document.getElementById("media").style.visibility = "visible";
+  }
+}
+
+// make the application fullscreen
+function full() {
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) {
+    document.documentElement.msRequestFullscreen();
+  }
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+// live search in searchbar
+function liveSearch(search, table) {
+  var input = document.getElementById(search);
+  var filter = input.value.toUpperCase();
+  var table = document.getElementById(table);
+  var tr = table.getElementsByTagName("tr");
+  for (var counter = 0; counter < tr.length; counter++) {
+    var td = tr[counter].getElementsByTagName("td")[0];
+    if (td) {
+      var txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[counter].style.display = "";
+      } else {
+        tr[counter].style.display = "none";
+      }
+    }
+  }
+}
+
+// login
+function login() {
+  // document.body.onclick
+  if (document.getElementById("login").style.visibility === "visible") {
+    document.body.style.backgroundColor = "LightCoral";
+    document.getElementById("login").style.borderBottom = "none";
+    document.getElementById("login").style.height = "0";
+    document.getElementById("login").style.visibility = "hidden";
+  } else {
+    document.body.style.backgroundColor = "#CD5C5C";
+    document.getElementsByTagName("a").disabled = true;
+    document.getElementById("login").style.borderBottom = "solid";
+    document.getElementById("login").style.height = "10%";
+    document.getElementById("login").style.visibility = "visible";
   }
 }
